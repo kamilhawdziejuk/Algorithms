c272a013097db774220d79d899369cccdfc06a88
KamilHawdziejuk <kamil.hawdziejuk@gmail.com>
2016-01-13 00:19:58 +0100

    Implementation of firstLowerOrEqual Implementation of Matrix operations

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -9,15 +9,47 @@ public class BinarySearch {
      * @param  key the search key
      * @return index of key in array <tt>a</tt> if present; <tt>-1</tt> otherwise
      */
-    public static int indexOf(int[] a, int key) {
+    public static int indexOfExactKey(int[] tab, int key) {
         int lo = 0;
-        int hi = a.length - 1;
+        int hi = tab.length - 1;
         while (lo <= hi) {
             // Key is in a[lo..hi] or not present.
             int mid = lo + (hi - lo) / 2;
-            if      (key < a[mid]) hi = mid - 1;
-            else if (key > a[mid]) lo = mid + 1;
+            if      (key < tab[mid]) hi = mid - 1;
+            else if (key > tab[mid]) lo = mid + 1;
             else return mid;
+        }
+        return -1;
+    }
+    
+    public static int indexOfFirstLowerOrEqualKey(int tab[], int key)
+    {
+    	int a = 0;
+        int b = tab.length - 1;
+        
+        if (tab[a] > key) return -1;
+        while (a <= b)
+        {
+            if (tab[b] <= key) return b;
+            if (tab[a] > key) 
+            {
+            	return a-1;
+            }
+    
+            int m = a + (b-a)/2;
+            int value = tab[m];
+            if (key < value)
+            {
+                b = m - 1;
+            }
+            else if (key > value)
+            {
+                a = m + 1;
+            }
+            else
+            {
+                return m;
+            }
         }
         return -1;
     }
