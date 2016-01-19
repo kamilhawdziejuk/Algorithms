25cb2261cc88810e64c83bd5c4b12005bca30c76
KamilHawdziejuk <kamil.hawdziejuk@gmail.com>
2016-01-20 00:25:46 +0100

    Fix comparable Edge implementation missing

--- src/Edge.java
+++ src/Edge.java
@@ -1,9 +1,19 @@
 import java.util.*;
 
-public class Edge
+
+public class Edge implements Comparable<Edge>
 {
     public final Vertex target;
     public final double weight;
     public Edge(Vertex argTarget, double argWeight)
     { target = argTarget; weight = argWeight; }
+    
+    @Override
+    public int compareTo(Edge e) {
+        if (this.weight < e.weight)
+            return -1;
+        if (this.weight > e.weight)
+            return 1;
+        return 0;
+    }
 }
