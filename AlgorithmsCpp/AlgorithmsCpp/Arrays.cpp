9dfb79c1c03c6c63e65e9a650936e7d5fc865af2
KamilHawdziejuk <kamil.hawdziejuk@gmail.com>
2016-08-05 18:19:13 +0200

    Arrays

---
+++ AlgorithmsCpp/AlgorithmsCpp/Arrays.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h"
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+
+class Arrays
+{
+public:
+	Arrays() {};
+	~Arrays() {};
+
+	//https://leetcode.com/problems/intersection-of-two-arrays/
+	vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+		unordered_map<int, int> hashmap;
+		for (auto i : nums1) {
+			hashmap[i] = 1;
+		}
+		for (auto i : nums2) {
+			if (hashmap.find(i) != hashmap.end()) {
+				hashmap[i] = 2;
+			}
+		}
+		vector<int> r;
+		for (auto i : hashmap) {
+			if (i.second == 2) {
+				r.push_back(i.first);
+			}
+		}
+		return r;
+	}
+};
