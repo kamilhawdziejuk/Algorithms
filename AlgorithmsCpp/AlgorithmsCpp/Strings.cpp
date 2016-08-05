9334bb7f0ee296291a3f827604d5e08bcc0acc5e
PLKAHAW <kamil.hawdziejuk@pl.abb.com>
2016-08-05 15:29:30 +0200

    https://leetcode.com/problems/compare-version-numbers/

--- AlgorithmsCpp/AlgorithmsCpp/Strings.cpp
+++ AlgorithmsCpp/AlgorithmsCpp/Strings.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include <string>
 #include <algorithm>
+#include <vector>
 
 using namespace::std;
 
@@ -15,6 +16,7 @@ class Strings
 			return str;
 		}
 
+		//https://leetcode.com/problems/reverse-string/
 		string reverseString(string str) {
 			int length = str.length();
 			int c, i, j;
@@ -26,5 +28,75 @@ class Strings
 				str[j] = c;
 			}
 			return str;
+		}
+
+		vector<string> split(string str, string delimiter)
+		{
+			vector<string> splits;
+			size_t pos = 0;
+			std::string token;
+
+			while ((pos = str.find(delimiter)) != std::string::npos) {
+				token = str.substr(0, pos);
+				splits.push_back(token);
+				str.erase(0, pos + delimiter.length());
+			}
+			splits.push_back(str);
+			return splits;
+		}
+
+		//https://leetcode.com/problems/compare-version-numbers/
+		int compareVersion(string version1, string version2) {
+			vector<string> v1 = split(version1, ".");
+			vector<string> v2 = split(version2, ".");
+
+			int s1 = v1.size();
+			int s2 = v2.size();
+
+			int i = 0;
+			while (i < s1 && i < s2)
+			{
+				int l1 = stoi(v1.at(i));
+				int l2 = stoi(v2.at(i));
+				if (l1 < l2)
+				{
+					return -1;
+				}
+				else if (l1 > l2)
+				{
+					return 1;
+				}
+				i++;
+			}
+			if (s1 == s2)
+			{
+				return 0;
+			}
+			else
+			{
+				if (s1 < s2)
+				{
+					while (i < s2)
+					{
+						int l2 = stoi(v2.at(i));
+						if (l2 != 0)
+						{
+							return -1;
+						}
+						i++;
+					}
+					return 0;
+				}
+				while (i < s1)
+				{
+					int l1 = stoi(v1.at(i));
+					if (l1 != 0)
+					{
+						return 1;
+					}
+					i++;
+				}
+				return 0;
+			}
 		}
 };
