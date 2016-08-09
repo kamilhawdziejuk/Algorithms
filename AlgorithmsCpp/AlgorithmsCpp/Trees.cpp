a3eed48be9ad5ed62d0fe8b90f11da2863b69bca
PLKAHAW <kamil.hawdziejuk@pl.abb.com>
2016-08-09 13:40:13 +0200

    lowestCommonAncestor

--- AlgorithmsCpp/AlgorithmsCpp/Trees.cpp
+++ AlgorithmsCpp/AlgorithmsCpp/Trees.cpp
@@ -18,6 +18,27 @@ private:
 public:
 	Trees() {};
 	~Trees() {};
+	
+	//https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+	TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) 
+	{
+		if (!root)
+		{
+			return NULL;
+		}
+		if (p->val < root->val && q->val < root->val)
+		{
+			lowestCommonAncestor(root->left, p, q);
+		}
+		else if (p->val > root->val && q->val > root->val)
+		{
+			lowestCommonAncestor(root->right, p, q);
+		}
+		else
+		{
+			return root;
+		}
+	}
 
 	//https://leetcode.com/problems/same-tree/
 	bool isSameTree(TreeNode* p, TreeNode* q)
