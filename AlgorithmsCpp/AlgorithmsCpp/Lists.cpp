416f8962eba65e66bfb97aa8a53aeba1b1b00210
Kamil Hawdziejuk <kamil.hawdziejuk@pl.abb.com>
2016-08-10 21:46:49 +0200

    lists algs

--- AlgorithmsCpp/AlgorithmsCpp/Lists.cpp
+++ AlgorithmsCpp/AlgorithmsCpp/Lists.cpp
@@ -18,6 +18,21 @@ class Lists
 	Lists() {};
 	~Lists() {};
 
+	//https://leetcode.com/problems/linked-list-cycle/
+	bool hasCycle(ListNode *head) {
+		if (head == NULL) return false;
+		ListNode* p1 = head->next;
+		ListNode* p2 = head->next != NULL ? head->next->next : NULL;
+
+		while (p1 != NULL && p2 != NULL && p1 != p2)
+		{
+			p1 = p1->next;
+			p2 = p2->next != NULL ? p2->next->next : NULL;
+		}
+		return (p1 != NULL && p2 != NULL && p1 == p2);
+	}
+
+    //https://leetcode.com/problems/delete-node-in-a-linked-list/
 	void deleteNode(ListNode* node) {
 
 		ListNode * nextNodeToDelete = node->next;
