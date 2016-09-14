25460138c269b9d577298cf91f8246faa59bf198
KamilHawdziejuk <kamil.hawdziejuk@gmail.com>
2016-09-14 22:09:23 +0200

    graph copy

---
+++ AlgorithmsCpp/AlgorithmsCpp/ApiContainers.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+#include <queue>
+#include <map>
+#include <functional>
+#include <set>
+
+ struct UndirectedGraphNode {
+     int label;
+     vector<UndirectedGraphNode *> neighbors;
+     UndirectedGraphNode(int x) : label(x) {};
+ };
+
+
+#pragma once
+class ApiContainers
+{
+public:
+	ApiContainers() {}
+	~ApiContainers() {}
+
+	//https://leetcode.com/problems/clone-graph/
+	unordered_map<UndirectedGraphNode*, UndirectedGraphNode*> map;
+	UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
+		if (!node) return NULL;
+		if (map.find(node) == map.end()) {
+			map[node] = new UndirectedGraphNode(node->label);
+			for (UndirectedGraphNode* neigh : node->neighbors)
+				map[node]->neighbors.push_back(cloneGraph(neigh));
+		}
+		return map[node];
+	}
+
+	void sets()
+	{
+		set<int> set1;
+		set1.insert(2);
+		set1.find(2);
+	}
+
+	void maps()
+	{
+		unordered_map<int, int> map;
+		map.insert(1, 2);
+		map.find(2);
+	}
+
+};
+
