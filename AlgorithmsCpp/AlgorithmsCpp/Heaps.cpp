f9ba1fea865e15a277bc179c6596b4a8c9f35fee
Kamil Hawdziejuk <kamil.hawdziejuk@pl.abb.com>
2016-12-14 08:20:30 +0100

    Heaps

--- AlgorithmsCpp/AlgorithmsCpp/Heaps.cpp
+++ AlgorithmsCpp/AlgorithmsCpp/Heaps.cpp
@@ -9,8 +9,108 @@
 
 using namespace std;
 
+class MinHeap
+{
+private:
+	vector<int> _vector;
+
+	void BubbleDown(int index)
+	{
+		int length = _vector.size();
+		int leftChildIndex = 2 * index + 1;
+		int rightChildIndex = 2 * index + 2;
+
+		if (leftChildIndex >= length)
+			return; //index is a leaf
+
+		int minIndex = index;
+
+		if (_vector[index] > _vector[leftChildIndex])
+		{
+			minIndex = leftChildIndex;
+		}
+
+		if ((rightChildIndex < length) && (_vector[minIndex] > _vector[rightChildIndex]))
+		{
+			minIndex = rightChildIndex;
+		}
+
+		if (minIndex != index)
+		{
+			//need to swap
+			int temp = _vector[index];
+			_vector[index] = _vector[minIndex];
+			_vector[minIndex] = temp;
+			BubbleDown(minIndex);
+		}
+	}
+	void BubbleUp(int index)
+	{
+		if (index == 0)
+			return;
+
+		int parentIndex = (index - 1) / 2;
+
+		if (_vector[parentIndex] > _vector[index])
+		{
+			int temp = _vector[parentIndex];
+			_vector[parentIndex] = _vector[index];
+			_vector[index] = temp;
+			BubbleUp(parentIndex);
+		}
+	}
+
+	void Heapify()
+	{
+		int length = _vector.size();
+		for (int i = length - 1; i >= 0; --i)
+		{
+			BubbleDown(i);
+		}
+	}
+
+public:
+	//MinHeap(int* array, int length);
+	MinHeap(const vector<int>& vector) : _vector(vector)
+	{
+		Heapify();
+	}
+
+	MinHeap() {}
+
+	void Insert(int newValue)
+	{
+		int length = _vector.size();
+		_vector[length] = newValue;
+
+		BubbleUp(length);
+	}
+
+	int GetMin()
+	{
+		return _vector[0];
+	}
+
+	void DeleteMin()
+	{
+		int length = _vector.size();
+
+		if (length == 0)
+		{
+			return;
+		}
+
+		_vector[0] = _vector[length - 1];
+		_vector.pop_back();
+
+		BubbleDown(0);
+	}
+};
+
+
 class Heaps
 {
+
 public:
 	Heaps() {};
 	~Heaps() {};
